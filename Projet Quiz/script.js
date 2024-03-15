d82b678145b912c665f2195c7e6f4d0838785df4
AurelienChetot <aurelien.chetot@gmail.com>
2024-03-15 12:02:23 +0100

    Rename HTML

--- Projet Quiz/script.js
+++ Projet Quiz/script.js
@@ -4,29 +4,29 @@ const timerButton = document.getElementById("display-start-quiz");
 const backGroundSize = document.getElementById("container-quiz");
 let temps = 10;
 
-    function time() {
-        timer.innerHTML = "Time Left : " + (temps < 10 ? "" : "") + temps;
-        if (temps > 0) {
-            temps--;
-        }
+function time() {
+    timer.innerHTML = "Time Left : " + (temps < 10 ? "" : "") + temps;
+    if (temps > 0) {
+        temps--;
+    }
 }
-    function startTimer() {
-        setInterval(time, 1000);
+function startTimer() {
+    setInterval(time, 1000);
 }    
-    document.querySelector("#next-question").addEventListener("click", function() {
-        temps = 10;
-    });
+document.querySelector("#next-question").addEventListener("click", function() {
+    temps = 10;
+});
 
-    function startQuiz() {
-        startQuizButton.style.display = "none";    
-        timerButton.style.justifyContent = "center";
-        timerButton.style.paddingLeft = "0px";
-        timer.style.width = "250px";
-        timer.style.fontSize = "1.9rem";
-        // backGroundSize.style.height = "39rem";
+function startQuiz() {
+    startQuizButton.style.display = "none";    
+    timerButton.style.justifyContent = "center";
+    timerButton.style.paddingLeft = "0px";
+    timer.style.width = "250px";
+    timer.style.fontSize = "1.9rem";
+    // backGroundSize.style.height = "39rem";
 
-        startTimer();
-        afficherQuestion(0);
+    startTimer();
+    afficherQuestion(0);
 }
 startQuizButton.addEventListener("click", startQuiz);
 
@@ -41,25 +41,52 @@ const questions = [{
     ]
 }];
 
+
 let optionQuestion = document.getElementById("question");
 let option1 = document.getElementById("op1");
 let option2 = document.getElementById("op2");
 let option3 = document.getElementById("op3");
 let option4 = document.getElementById("op4");
 
-    function afficherQuestion(idQuestion) {
-        const realQuestion = questions[idQuestion];
-        optionQuestion.innerHTML = realQuestion.q;
+function afficherQuestion(idQuestion) {
+    const realAnswer = questions[idQuestion];
+    optionQuestion.innerHTML = realAnswer.q;
         
-        option1.textContent = realQuestion.a[0].text;
-        option2.textContent = realQuestion.a[1].text;
-        option3.textContent = realQuestion.a[2].text;
-        option4.textContent = realQuestion.a[3].text;
+    option1.textContent = realAnswer.a[0].text;
+    option2.textContent = realAnswer.a[1].text;
+    option3.textContent = realAnswer.a[2].text;
+    option4.textContent = realAnswer.a[3].text;
+}
+// afficherQuestion(0);
+
+const answersButton = document.querySelectorAll(".answer-btn");
+function answerColor (idQuestion) {
+    const realAnswer2 = questions[idQuestion];
+    
+    const true0 = realAnswer2.a[0].correct;
+    const true1 = realAnswer2.a[1].correct;
+    const true2 = realAnswer2.a[2].correct;
+    const true3 = realAnswer2.a[3].correct;
+
+    if (true0 === questions) {
+        answersButton.style.backgroundColor ="green";
     }
-    // afficherQuestion(0);
+    answersButton.addEventListener("click", answerColor);
+}
 
 
-const prochaineQuestion = document.getElementById("next-question");
+
+
+
+// a.addEventListener("click");
+
+
+// option1.addEventListener("click", () => {
+//     option1.style.backgroundColor = "green";
+//     option2.style.backgroundColor = "red";
+//     option3.style.backgroundColor = "red";
+//     option4.style.backgroundColor = "red";
+// })
 
 
 
